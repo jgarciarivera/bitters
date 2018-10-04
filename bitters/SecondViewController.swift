b1124eb9422e320ffa14b2cdcb4ab5253fa50b73
Jorge Garcia-Rivera <jorge.garcia-rivera@p97.com>
2018-10-03 22:25:50 -0500

    Initial Commit

---
+++ bitters/SecondViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SecondViewController.swift
+//  bitters
+//
+//  Created by Jorge Garcia-Rivera on 10/3/18.
+//  Copyright © 2018 Jorge Garcia-Rivera. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
