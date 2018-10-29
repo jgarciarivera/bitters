4c4fbcf5ddca0baa62e515adad85051fce94276e
Luis Flores <24.flores.luis@gmail.com>
2018-10-29 16:43:51 -0500

    Initial UI for Inventory

---
+++ bitters/Inventory/InventoryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  InventoryTableViewCell.swift
+//  bitters
+//
+//  Created by Luis Flores on 10/29/18.
+//  Copyright © 2018 Jorge Garcia-Rivera. All rights reserved.
+//
+
+import UIKit
+
+class InventoryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
