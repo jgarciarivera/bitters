8a6c3adcbf93219e5533a7bd66d9a385f5e3a1df
Jorge Garcia-Rivera <jorge.garcia-rivera@p97.com>
2018-10-31 00:30:42 -0500

    Refresh UI and add registration screen with basic error handling.

---
+++ bitters/LoginViewController.swift
@@ -0,0 +1,50 @@
+//  LoginPageViewController.swift
+//  bitters
+//
+//  Created by Kristian Galvan on 10/25/18.
+//  Copyright © 2018 Jorge Garcia-Rivera. All rights reserved.
+
+import UIKit
+import Firebase
+import FirebaseAuth
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loginButton.layer.cornerRadius = 16
+        loginButton.clipsToBounds = true
+    }
+    
+    @IBAction func loginButton(_ sender: Any) {
+        guard let email = emailTextField.text else { return }
+        guard let password = passwordTextField.text else { return }
+        
+        Auth.auth().signIn(withEmail: email, password: password) { (user, error) in
+            if let error = error {
+                print("Error logging in: \(error.localizedDescription)")
+            } else if Auth.auth().currentUser != nil {
+                print("Successful log in!")
+                self.performSegue(withIdentifier: "loginToMain", sender: self)
+            }
+        }
+    }
+    
+    @IBAction func registerNewAccount(_ sender: Any) {
+        performSegue(withIdentifier: "loginToRegister", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "loginToMain" {
+            let destinationViewController = segue.destination as! UITabBarController
+        } else if segue.identifier == "loginToRegister" {
+            let destinationViewController = segue.destination as! RegisterViewController
+        } else {
+            print("Could not find corresponding segue")
+        }
+    }
+}
---
+++ bitters/RegisterViewController.swift
@@ -0,0 +1,57 @@
+//  SignUpViewController.swift
+//  bitters
+//
+//  Created by Jorge Garcia-Rivera on 10/29/18.
+//  Copyright © 2018 Jorge Garcia-Rivera. All rights reserved.
+
+import UIKit
+import Firebase
+
+class RegisterViewController: UIViewController {
+    
+    @IBOutlet weak var registerButton: UIButton!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var confirmationTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        registerButton.layer.cornerRadius = 16
+        registerButton.clipsToBounds = true
+    }
+    
+    @IBAction func completeRegistration(_ sender: Any) {
+        if (usernameTextField.hasText && emailTextField.hasText && passwordTextField.hasText && confirmationTextField.hasText) {
+            let email = emailTextField.text!
+            let password = passwordTextField.text!
+            let confirmation = confirmationTextField.text!
+            
+            if (password == confirmation && password.count >= 6) {
+                Auth.auth().createUser(withEmail: email, password: password) { (user, error) in
+                    if let error = error {
+                        print("Error attempting to register user: \(error.localizedDescription)")
+                    } else {
+                        self.performSegue(withIdentifier: "registerToMain", sender: self)
+                    }
+                }
+            } else {
+                generateAlert(alertTitle: "Password", alertMessage: "The password must be at least 6 characters long and match the password confirmation")
+            }
+        } else {
+            generateAlert(alertTitle: "Missing Information", alertMessage: "Please ensure you enter all required information")
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if (segue.identifier == "registerToMain") {
+            let destinationViewController = segue.destination as! UITabBarController
+        }
+    }
+    
+    func generateAlert(alertTitle: String, alertMessage: String) {
+        let alert = UIAlertController(title: alertTitle, message: alertMessage, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Okay", style: .default, handler: {_ in alert.dismiss (animated: true, completion: nil )}))
+        self.present(alert, animated: true, completion: nil)
+    }
+}
