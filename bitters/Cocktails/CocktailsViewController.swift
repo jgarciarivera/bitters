4174df76faeb45e212be31e79bed561e95b5e56a
Jorge Garcia-Rivera <jorge.garcia-rivera@p97.com>
2018-11-06 21:12:26 -0600

    Add cocktails, discover, shopping, and favorites tab bar items.

---
+++ bitters/Cocktails/CocktailsViewController.swift
@@ -0,0 +1,14 @@
+//  CocktailsViewController.swift
+//  bitters
+//
+//  Created by Jorge Garcia-Rivera on 11/4/18.
+//  Copyright © 2018 Jorge Garcia-Rivera. All rights reserved.
+
+import UIKit
+
+class CocktailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
