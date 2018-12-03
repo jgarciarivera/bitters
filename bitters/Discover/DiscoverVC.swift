6359a2d9ed9da9539ef3d955dd0e5057314e6b5a
kgalvan170 <kristian.galvan@gmail.com>
2018-12-02 21:02:21 -0600

    scroll view and featured image and collection view

--- bitters/Discover/DiscoverVC.swift
+++ bitters/Discover/DiscoverVC.swift
@@ -8,27 +8,51 @@
 
 import UIKit
 
-class DiscoverVC: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
-    let items:[String] = ["0", "1", "2", "3", "4"]
+class DiscoverVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    //let items:[String] = ["0", "1", "2", "3", "4"]
+    
+    @IBOutlet weak var featuredImage: UIImageView!
+    
+    
+    
+    var imageArrayss = [UIImage(named: "0"),UIImage(named: "1"),UIImage(named: "2"),UIImage(named: "3"),UIImage(named: "4"),]
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
+   
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return items.count
+
+        // may have to change this
+
+        return imageArrayss.count
     }
-    
+
     //Populate view
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell
     {
-       let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "recentCell", for: indexPath) as! CollectionViewCell
-        cell.recentImageView.image = UIImage(named: items[indexPath.row] + ".png")
+       let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "recentImageCell", for: indexPath) as! imageCollectionViewCell
+
+        // load the image
+        cell.recentImage.image = imageArrayss[indexPath.row]
+        
+        // load the label
+        
+        //design the cell
+        cell.layer.cornerRadius = 6.0
+        cell.layer.masksToBounds = false
+        cell.layer.shadowColor = UIColor.gray.cgColor
+        cell.layer.shadowOffset = CGSize(width: 0, height: 1)
+        cell.layer.shadowOpacity = 0.6
+        
+     
+       // print(cell.recentImage.image ?? 0)
         return cell
     }
 }
+
---
+++ bitters/Discover/imageCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  imageCollectionViewCell.swift
+//  
+//
+//  Created by COMMStudent on 12/1/18.
+//
+
+import UIKit
+
+class imageCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var recentImage: UIImageView!
+    
+}
