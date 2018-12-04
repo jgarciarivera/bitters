1e10d26f516bbc65ca81208755e066e318ca5080
kgalvan170 <kristian.galvan@gmail.com>
2018-12-04 13:49:14 -0600

    min UI working

--- bitters/Discover/DiscoverVC.swift
+++ bitters/Discover/DiscoverVC.swift
@@ -7,24 +7,68 @@
 //
 
 import UIKit
+import CoreMotion
+
+
 
 class DiscoverVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     //let items:[String] = ["0", "1", "2", "3", "4"]
     
+    var shadowBlur: CGFloat = 14
+    var shadowOpacity: Float = 0.6
+    var shadowColor: UIColor = UIColor.gray
+    var cardRadius: CGFloat = 20
+    var contentInset: CGFloat = 6
+    
+  
     @IBOutlet weak var featuredImage: UIImageView!
+ 
+    @IBOutlet weak var featuredView: UIView!
+    @IBOutlet weak var CollectionViewRecent: UICollectionView!
     
-    private weak var shadowView: UIView?
-    
-    var imageArrayss = [UIImage(named: "0"),UIImage(named: "1"),UIImage(named: "2"),UIImage(named: "3"),UIImage(named: "4"),]
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        featuredView.layer.cornerRadius = 20.0 //not working
+        featuredView.layer.borderWidth = 1.0
+        featuredView.layer.borderColor = UIColor.clear.cgColor
+        featuredView.layer.masksToBounds = false
+        featuredView.layer.shadowColor = UIColor.gray.cgColor
+        featuredView.layer.shadowOffset = CGSize(width: 0, height: 0)
+        featuredView.layer.shadowRadius = 12.0
+        featuredView.layer.shadowOpacity = 1
+        
 
+        featuredImage.layer.cornerRadius = 20.0
+        featuredImage.image = imageArrayss[0]
+        featuredImage.contentMode = .scaleAspectFill
+        featuredImage.layer.masksToBounds = true
+        
+         //featuredImage.layer.shadowPath = UIBezierPath(roundedRect: featuredImage.bounds, cornerRadius: featuredImage.layer.cornerRadius).cgPath
+        
+        
+//        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+//
+//        //layout.sectionInset = UIEdgeInsets(top: 20, left: 2, bottom: 10, right: 2)
+//        layout.minimumInteritemSpacing = 20
+        //layout.minimumLineSpacing = 0
+       // CollectionViewRecent!.collectionViewLayout.layou
+        
+        
+//        CollectionViewRecent.layer.cornerRadius = 20.0
+//        CollectionViewRecent.layer.shadowColor = UIColor.gray.cgColor
+//        CollectionViewRecent.layer.shadowOffset = CGSize.zero
+//        CollectionViewRecent.layer.shadowRadius = 12.0
+//        CollectionViewRecent.layer.shadowOpacity = 0.7
         // Do any additional setup after loading the view.
     }
-   
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
+    var imageArrayss = [UIImage(named: "0"),UIImage(named: "1"),UIImage(named: "2"),UIImage(named: "3"),UIImage(named: "4"),]
+    
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
 
@@ -38,38 +82,45 @@ class DiscoverVC: UIViewController, UICollectionViewDelegate, UICollectionViewDa
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell
     {
        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "recentImageCell", for: indexPath) as! imageCollectionViewCell
-
-    
+        
         cell.recentImage.image = imageArrayss[indexPath.row]
         
+       
+//////////////
         
-        cell.layer.shadowOffset = CGSize(width: 0, height: 1)
-  
+//        cell.contentView.layer.cornerRadius = 15.0 //not working
+//        cell.contentView.layer.borderWidth = 1.0
+//        cell.contentView.layer.borderColor = UIColor.clear.cgColor
+        //cell.contentView.layer.masksToBounds = true
+        
+        cell.layer.borderWidth  = 1.0
+        cell.layer.borderColor = UIColor.clear.cgColor
+        cell.layer.cornerRadius = 15.0
+        cell.layer.shadowColor = UIColor.gray.cgColor
+        cell.layer.shadowOffset = CGSize(width: 0, height: 0)
+        cell.layer.shadowRadius = 6.0 // test
+        cell.layer.shadowOpacity = 1 // test
         cell.layer.masksToBounds = false
-        cell.layer.shadowRadius = 8.0
-        cell.layer.shadowColor = UIColor.black.cgColor
+     
+        cell.recentImage.layer.cornerRadius = 15.0
+        cell.recentImage.clipsToBounds = true
+        cell.recentImage.contentMode = .scaleAspectFill
         
         
-        cell.layer.shadowOpacity = 0.35
-      
+        // just in case
+        //cell.layer.shadowPath = UIBezierPath(roundedRect: cell.bounds, cornerRadius: cell.contentView.layer.cornerRadius).cgPath
+       
+        
+    
         return cell
     }
     
-    //ui
     internal func setHeaderHeight(_ height: CGFloat) {
         //featuredImage.constant = height
         view.layoutIfNeeded()
     }
     
-    internal func configureRoundedCorners(shouldRound: Bool) {
-        featuredImage.layer.cornerRadius = shouldRound ? 14.0 : 0.0
-    }
     
-    //ui end
-    
-    // testing
- 
-    
-   
 }
+
 
--- bitters/Discover/imageCollectionViewCell.swift
+++ bitters/Discover/imageCollectionViewCell.swift
@@ -9,6 +9,24 @@ import UIKit
 
 class imageCollectionViewCell: UICollectionViewCell {
     
-    @IBOutlet weak var recentImage: UIImageView!
+    @IBOutlet weak var recentImage: UIImageView! {
+        didSet {
+            let gradient = CAGradientLayer()
+            gradient.colors = [UIColor(red: 0, green: 0, blue: 0, alpha: 0.6).cgColor, UIColor.clear.cgColor]
+            gradient.locations = [0.0, 1.0]
+            
+            gradient.startPoint = CGPoint(x: 0, y: 1)
+            gradient.endPoint = CGPoint(x: 0, y: 0)
+            gradient.frame = CGRect(x: 0,
+                                    y: 0,
+                                    width: UIScreen.main.bounds.width,
+                                    height: recentImage.bounds.height)
+            
+            recentImage.layer.insertSublayer(gradient, at: 0)
+            recentImage.contentMode = .scaleAspectFill
+            recentImage.clipsToBounds = true
+        }
+    }
+    
     
 }
